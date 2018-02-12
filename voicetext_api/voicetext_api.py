4e487fefe7cf533d3871d1a9da20a1b7bb94f852
hsuallan <33412402+hsuallan@users.noreply.github.com>
2018-02-12 21:07:41 +0800

    Ver 1.0 finish ver1.0 content 基本的方法加入 要更新的話 -例外處裡跟debug用的訊息 -簡易輸入內容正確於否判斷 -讓我想想

--- voicetext_api/voicetext_api.py
+++ voicetext_api/voicetext_api.py
@@ -4,7 +4,7 @@ import pygame
 
 class viocetext:
     _URL = 'https://api.voicetext.jp/v1/tts'#post request url
-    _dict = {"text":"","speaker":"hikari","format":"mp3","emotion":"happiness","emotion_level":"2","pitch":"100","speed":"100","volume":"100"}#傳送的資料
+    _dict = {"text":"","speaker":"hikari","format":"mp3","emotion":None,"emotion_level":None,"pitch":"100","speed":"100","volume":"100"}#傳送的資料
     def __init__(self,api_key):
         self._auth = HTTPBasicAuth(api_key, api_key)
     def speak(self,text):
@@ -17,21 +17,40 @@ class viocetext:
         pygame.mixer.music.load("temp.mp3")
         pygame.mixer.music.play()
     def set_speaker(self,speaker):
-        print("")
+        self._dict["speaker"] = speaker
     def set_emotion(self,emption):
-        print("")
+        self._dict["emption"] = emption
     def set_emotion_level(self,level):
-        print("")
+        self._dict["emotion_level"] = level
     def set_pitch(self,pitch):
-        print()
+         if pitch>200:
+            pitch = 200
+         elif pitch< 50:
+             pitch = 50
+         self._dict["pitch"] = pitch
     def set_speed(self,speed):
-        print()
+        if speed>200:
+            speed = 200
+        elif speed<50:
+            speed = 50
+        self._dict["speed"] = speed
     def set_volume(self,vol):
-        print()
+        if vol>200:
+            vol = 200
+        elif vol<50:
+            vol = 50
+        self._dict["vol"] = vol
     def save_as_wav(self,text):
-        print()
-a = viocetext("zkoiea4yphm49iyn")
-a.speak("おはよう")
+        self._dict["text"] = text
+        self._dict["format"] = "wav"
+        self._get = requests.post(self._URL,data =self._dict ,auth=self._auth)
+        self._tempfile = open(text+".wav","wb",buffering = 1000000000)
+        self._tempfile.write(self._get.content)
+        self._tempfile.close()
+        print("successfully save as"+text+".wav")
+
+
+
         
 
 
